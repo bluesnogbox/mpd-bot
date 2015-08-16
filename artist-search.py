83c58ff3917093aef173bb1a0f6cd61c67847a49
Taylor <mybluesnogbox@gmail.com>
2015-08-16 18:03:15 +0000

    add basic artist search

---
+++ artist-search.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+
+from mpd import MPDClient
+import sys
+
+artist = raw_input("Please search for an artist: ")
+client = MPDClient()               # create client object
+client.timeout = 10                # network timeout in seconds (floats allowed), default: None
+client.idletimeout = None          # timeout for fetching the result of the idle command is handled seperately, default: None
+client.connect("bluesnogbox.duckdns.org", 6600)  # connect to URL:6600 (default port)
+
+print(client.mpd_version)          # print the MPD version
+print(client.search("artist", artist))
+
+client.close()                     # send the close command
+client.disconnect()                # disconnect from the server
